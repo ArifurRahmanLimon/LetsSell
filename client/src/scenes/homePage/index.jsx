64157b9c7450fa0a9fd2c1ed8ba0c09ebac56ec5
Limon <arifurlimon88@gmail.com>
2022-12-09 14:23:43 +0600

    Added code related to profile page

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,9 +1,43 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery} from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
 const HomePage = () => {
+
+    const isNonMobileSreens = useMediaQuery("(min-width : 1000px)");
+    // const { _id, picturePath } = useSelector((state) => state.user);
+    const picturePath  = "p11.jpeg";
+    const _id = '1';
+
+    console.log(`Limon userId : ${_id}`);
+
     return(
        <Box>
         <Navbar/>
+
+        <Box 
+        width = "100%"
+        padding = "2rem 6%"
+        display={isNonMobileSreens ? "flex" : "block"}
+        gap = "0.5rem"
+        justifyContent="space-between"
+        >
+
+            <Box flexBasis={isNonMobileSreens ? "26%" : undefined}>
+                <UserWidget userId={_id} picturePath = {picturePath} />
+            </Box>
+
+            <Box
+             flexBasis={isNonMobileSreens ? "42%" : undefined}
+             mt = {isNonMobileSreens ? undefined : "2rem"}
+            >
+
+            </Box>
+            {isNonMobileSreens && (
+                <Box flexBasis="26%">
+                </Box> 
+            )}
+        </Box>
        </Box>
     )
 }
