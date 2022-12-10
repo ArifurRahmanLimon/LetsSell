363e07120f76797a3e726c3d17bd709631ccfdd8
Limon <arifurlimon88@gmail.com>
2022-12-10 12:39:10 +0600

    Asset and picture issue fixed

--- Server/index.js
+++ Server/index.js
@@ -31,7 +31,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy : "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({  limit : "30mb", extended : true}));
 app.use(cors());
-app.use("/asset", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 
 
